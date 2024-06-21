8307b37bacd74abdfb673ee6d3c2532423554227
Amaiyo Praises <amaiyo.praises@gmail.com>
2024-06-21 19:42:58 +0100

    Update on the Categories section

--- src/features/homepage/FeaturedCategories.jsx
+++ src/features/homepage/FeaturedCategories.jsx
@@ -2,9 +2,14 @@ import styled from 'styled-components';
 import Heading from '../../ui/Heading';
 import Row from '../../ui/Row';
 
-import recommendedImage5 from '../../assets/images/recommended-img-5.png';
-import recommendedImage2 from '../../assets/images/recommended-img-2.png';
-import recommendedImage4 from '../../assets/images/recommended-img-4.png';
+import featuredCategoryImage1 from '../../assets/images/categories-1.png';
+import featuredCategoryImage2 from '../../assets/images/categories-2.png';
+import featuredCategoryImage3 from '../../assets/images/categories-3.png';
+import featuredCategoryImage4 from '../../assets/images/categories-4.png';
+import featuredCategoryImage6 from '../../assets/images/categories-7.png';
+import featuredCategoryImage7 from '../../assets/images/categories-8.png';
+import featuredCategoryImage8 from '../../assets/images/categories-9.png';
+import featuredCategoryImage9 from '../../assets/images/categories-11.png';
 
 const StyledFeaturedCategories = styled.section`
   padding: 4rem 0;
@@ -13,6 +18,7 @@ const StyledFeaturedCategories = styled.section`
 const FeaturedCategoriesContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
+  gap: 1rem;
 `;
 
 const FeaturedCategory = styled.div`
@@ -20,13 +26,17 @@ const FeaturedCategory = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  height: 15rem;
+  width: 15rem;
 
-  /* img {
-    height: 20rem;
-    width: 20rem;
+  div {
+    border-radius: var(--border-radius-round);
+    padding: 2rem;
     background-color: var(--color-gold-50);
-    padding: 5rem;
-  } */
+    display: flex; /* Ensure flexbox is used */
+    align-items: center; /* Center items vertically */
+    justify-content: center; /* Center items horizontally */
+  }
 
   span {
     font-size: 1.2rem;
@@ -36,11 +46,8 @@ const FeaturedCategory = styled.div`
 const ProductImage = styled.img`
   width: 100%;
   height: auto;
-  /* object-fit: cover;
-  object-position: center; */
-  background-color: var(--color-gold-50);
-  border-radius: var(--border-radius-round);
-  padding: 5rem;
+  object-fit: cover;
+  object-position: center;
 `;
 
 function FeaturedCategories() {
@@ -51,7 +58,51 @@ function FeaturedCategories() {
 
         <FeaturedCategoriesContainer>
           <FeaturedCategory>
-            <ProductImage src={recommendedImage2} alt="Category one" />
+            <div>
+              <ProductImage src={featuredCategoryImage1} alt="Category one" />
+            </div>
+          </FeaturedCategory>
+
+          <FeaturedCategory>
+            <div>
+              <ProductImage src={featuredCategoryImage2} alt="Category two" />
+            </div>
+          </FeaturedCategory>
+
+          <FeaturedCategory>
+            <div>
+              <ProductImage src={featuredCategoryImage3} alt="Category three" />
+            </div>
+          </FeaturedCategory>
+
+          <FeaturedCategory>
+            <div>
+              <ProductImage src={featuredCategoryImage4} alt="Category four" />
+            </div>
+          </FeaturedCategory>
+
+          <FeaturedCategory>
+            <div>
+              <ProductImage src={featuredCategoryImage6} alt="Category six" />
+            </div>
+          </FeaturedCategory>
+
+          <FeaturedCategory>
+            <div>
+              <ProductImage src={featuredCategoryImage7} alt="Category seven" />
+            </div>
+          </FeaturedCategory>
+
+          <FeaturedCategory>
+            <div>
+              <ProductImage src={featuredCategoryImage8} alt="Category eight" />
+            </div>
+          </FeaturedCategory>
+
+          <FeaturedCategory>
+            <div>
+              <ProductImage src={featuredCategoryImage9} alt="Category nine" />
+            </div>
           </FeaturedCategory>
         </FeaturedCategoriesContainer>
       </Row>
